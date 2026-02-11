c6bd79b1ac7be1ac44135d25993d4fbec478ee46
Ezequiel Lino <sonisombra@gmail.com>
2026-02-11 00:06:32 -0600

    peticion hacia giphy

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -4,6 +4,7 @@ import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { mockGifs } from "./mock-data/gifs.mock";
 import CustomHeader from "./shared/components/CustomHeader";
 import SearchBar from "./shared/components/SearchBar";
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.actions";
 
 export default function GifsApp() {
 
@@ -14,13 +15,17 @@ export default function GifsApp() {
 
     }
 
-    const handleSearch = (query: string = '') => {
+    const handleSearch = async (query: string = '') => {
         query = query.trim().toLowerCase();
 
         if (query.length === 0) return;
         if (previousTerm.includes(query)) return;
 
         setPreviousTerm([query, ...previousTerm].splice(0, 8));
+
+        const gifs = await getGifsByQuery(query)
+        console.log(gifs);
+
     }
 
     return (
