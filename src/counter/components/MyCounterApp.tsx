8afa01c1697b424abc22246ddc0de61e4c6092b6
Ezequiel Lino <sonisombra@gmail.com>
2026-02-12 13:38:41 -0600

    custom hook

---
+++ src/counter/components/MyCounterApp.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import { Onclick } from '../../gifs/interfaces/giphy.response';
+
+export const MyCounterApp = () => {
+  const [counter, setCounter] = useState(5);
+
+  const handleAdd = () => {
+    setCounter(counter + 1)
+  }
+
+  const handleSubtract = () => {
+    if (counter === 1) return
+
+    setCounter(prevState => prevState - 1)
+  }
+
+  const handleReset = () => {
+    setCounter(5)
+  }
+
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <h1>Counter: {counter}</h1>
+
+      <div style={{ display: 'flex', gap: '10px' }}>
+
+        <button
+          onClick={handleAdd}
+        >+1</button>
+        <button
+          onClick={handleSubtract}
+        >-1</button>
+
+        <button
+          onClick={handleReset}
+        >Reset</button>
+
+      </div>
+    </div>
+  )
+}
