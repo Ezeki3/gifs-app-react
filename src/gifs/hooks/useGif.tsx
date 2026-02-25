bf6ffe5aa8d4e06eb41d95e05d8fef515ad75324
Ezequiel Lino <sonisombra@gmail.com>
2026-02-25 17:49:04 -0600

    pruebas sobre useGifs - cache

--- src/gifs/hooks/useGif.tsx
+++ src/gifs/hooks/useGif.tsx
@@ -18,7 +18,8 @@ export const useGif = () => {
     }
 
     const gifs = await getGifsByQuery(term);
-    setGifs(gifs)
+    setGifs(gifs);
+    gifsCache.current[term] = gifs;
   }
 
   const handleSearch = async (query: string = '') => {
@@ -33,7 +34,6 @@ export const useGif = () => {
     setGifs(gifs);
 
     gifsCache.current[query] = gifs;
-    console.log(gifsCache);
 
   }
   return {
